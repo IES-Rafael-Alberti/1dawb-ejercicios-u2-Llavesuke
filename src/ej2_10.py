95c74eb88605d6221dde1fb8d315c0354275744e
llavesuke <llavesukeprogram@gmail.com>
2023-10-29 01:20:13 +0200

    Tarea 2.1 Finalizada

---
+++ src/ej2_10.py
@@ -0,0 +1,45 @@
+
+def Ask_User():
+    return int(input('Quieres una pizza vegetariana (1)/ pizza no vegetariana(2) -> '))
+
+def Clasify(selection):
+    match selection:
+        case 1:
+            return 'vegetariana'
+        case 2:
+            return 'no vegetariana'
+
+def Elements(clasification):
+    match clasification:
+        case 'vegetariana':
+            list = ['Pimiento (1)', 'tofu (2)']
+            joinedList= ','.join(list)
+
+            answer = int(input(f'Ingredientes vegetarianos: {joinedList} -> '))
+
+            list = list[answer-1].split(' ')
+
+            return list[0].lower()
+
+        case 'no vegetariana':
+            list = ['Peperoni (1)', 'Jamon (2)', 'Salmon (3)']
+            joinedList= ','.join(list)
+
+            answer = int(input(f'Ingredientes vegetarianos: {joinedList} -> '))
+
+            list = list[answer-1].split(' ')
+
+            return (list[0]).lower()
+
+
+
+def main():
+    selection = Ask_User()
+    clasification = Clasify(selection)
+    chosenElement = Elements(clasification)
+
+    print(f'\nTu pizza elegida es {clasification}')
+    print(f'Los ingredientes son tomate, mozzarella, {chosenElement}')
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_ej2_10.py
@@ -0,0 +1,31 @@
+import pytest
+from src.ej2_10 import Ask_User, Clasify, Elements
+
+def test_Ask_User(monkeypatch):
+    monkeypatch.setattr('builtins.input', lambda _: 1)
+    result = Ask_User()
+    assert result == 1
+
+
+@pytest.mark.parametrize(
+    'input, expected',
+    [
+        (1,'vegetariana'),
+        (2,'no vegetariana')
+    ]
+)
+def test_Clasify_params(input,expected):
+    assert Clasify(input) == expected
+
+@pytest.mark.parametrize(
+    'imported_input, expected',
+    [
+        (1,'pimiento'),
+        (2,'tofu'),
+        (1,'peperoni'),
+        (2,'jamon'),
+        (3,'salmon')
+    ]
+)
+def test_Elements_params(imported_input,expected):
+    Elements(imported_input) == expected
